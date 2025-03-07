ceed4b9d0bd6eae4c5f8e8cb57236edc4a8e6594
Pruthuvi Fernando <pruthuvi.fernando@gmail.com>
2025-03-07 23:48:24 +0530

    Complete newDeck function
    - use nested for loops to iterate through suits and values to generate a slice of type deck
    - use _ to ignore the variables that aren't used inside the for loop

--- cards/main.go
+++ cards/main.go
@@ -1,22 +1,7 @@
 package main
 
 func main() {
-	// card := "Ace of Spades"
-	// card := newCard()
-	cards := deck{"Ace of Spades", newCard()}
-	cards = append(cards, "Six of Spades")
-
-	// for i, card := range cards {
-	// 	fmt.Println(i, card)
-	// }
-
-	// for i := range cards {
-	// 	fmt.Println(cards[i])
-	// }
+	cards := newDeck()
 
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
